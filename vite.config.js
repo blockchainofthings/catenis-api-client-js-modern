264bdc13f21cb84777144daa11d084f5f7ae43e4
Cláudio Sá de Castro <claudio@hiades.com.br>
2023-01-11 18:28:54 -0300

    Update the build process so both plain and minified versions of the library are created.

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,31 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    build: {
-        target: 'modules',
-        modulePreload: false,
-        lib: {
-            entry: resolve(__dirname, 'main.js'),
-            formats: [
-                'es'
-            ],
-            fileName: 'catenis-api-client'
-        },
-        sourcemap: false,
-        minify: false
+export default defineConfig(({command, mode}) => {
+    if (command === 'build') {
+        const config = {
+            build: {
+                target: 'modules',
+                modulePreload: false,
+                lib: {
+                    entry: resolve(__dirname, 'main.js'),
+                    formats: [
+                        'es'
+                    ],
+                    fileName: 'catenis-api-client'
+                },
+                outDir: 'dist/plain',
+                sourcemap: false,
+                minify: false
+            }
+        };
+
+        if (mode === 'minified') {
+            config.build.lib.fileName = 'catenis-api-client.min';
+            config.build.outDir = 'dist/minified';
+            config.build.minify = true;
+        }
+
+        return config;
     }
 });
